f43850804e433f63a784b66b9df9432eb7f1b50b
Prince22378 <121478109+Prince22378@users.noreply.github.com>
2025-03-24 16:09:50 +0530

    fixing imports

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,4 +1,4 @@
-from api.models import User, Profile, ChatMessage, FriendRequest
+from api.models import User, Profile, ChatMessage, FriendRequest, Post
 from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
