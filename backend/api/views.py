14b17580b6885307727a8cd89194547c66e4b0a7
Saurav <saurav22464@iiitd.ac.in>
2025-03-25 17:24:36 +0530

    removed some unnecessary routes

--- backend/api/views.py
+++ backend/api/views.py
@@ -176,34 +176,6 @@ class ToggleUserVerification(APIView):
             return Response({"error": "Profile not found"}, status=status.HTTP_404_NOT_FOUND)
     
 
-class MyInbox(generics.ListAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = MessageSerializer
-
-    def get_queryset(self):
-        user_id = self.kwargs['user_id']
-        
-        if str(self.request.user.id) != user_id:
-            raise PermissionDenied("You can only view your own inbox.")
-
-        messages = ChatMessage.objects.filter(
-            id__in=Subquery(
-                User.objects.filter(
-                    Q(sender__reciever=user_id) |
-                    Q(reciever__sender=user_id)
-                ).distinct().annotate(
-                    last_msg=Subquery(
-                        ChatMessage.objects.filter(
-                            Q(sender=OuterRef('id'), reciever=user_id) |
-                            Q(reciever=OuterRef('id'), sender=user_id)
-                        ).order_by('-id')[:1].values_list('id', flat=True)
-                    )
-                ).values_list('last_msg', flat=True).order_by("-id")
-            )
-        ).order_by("-id")
-
-        return messages
-
 
 class GetMessages(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
